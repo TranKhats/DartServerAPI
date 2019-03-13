3a396a595e2f6121ed1f2613fb0edc6741da579a
Tran Kha <35250985+TranKhats@users.noreply.github.com>
2019-03-13 10:54:43 +0700

    checkPHPadmin

--- webserver/bin/server.dart
+++ webserver/bin/server.dart
@@ -1,32 +1,26 @@
 import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
-//import 'package:mysql1/mysql1.dart';
+import 'package:mysql1/mysql1.dart';
 
-    // $servername = "localhost";
-    // $username = "root";
-    // $password = "";
-    // $dbname = "servicereact";
-Map jsonData = {
-  'name': 'Tran kha',
-  'job': 'reluctant hero',
-  'BFF': 'Chewbacca',
-  'ship': 'God ship',
-  'weakness': 'smuggling debts'
-};
-
-// var settings=new ConnectionSettings(
-//   host: 'localhost',
-//   port: 8080,
-//   user: 'Khoikhats123',
-//   password: '',
-//   db: 'test'
-// );
-
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
 
 Future main() async {
-  //var  conn=await MySqlConnection.connect(settings);
-  //var result=await conn.query('select * from SinhVien');
+ var settings = new ConnectionSettings(
+  host: '207.148.71.41', 
+  port: 3306,
+  user: 'root',
+  password: 'Khoikhats123',
+  db: 'test'
+);
+var conn = await MySqlConnection.connect(settings);
+  var result = await conn.query('select * from SinhVien');
+  // for (var i = 0; i < result.toList().length; i++) {
+    
+  // }
+  
   var server = await HttpServer.bind(
     InternetAddress.loopbackIPv4,
     8081,
@@ -34,12 +28,10 @@ Future main() async {
   print('Listening on localhost:${server.port}');
 
   await for (HttpRequest request in server) {
-    if(request.method=='GET')
-    {
+    if (request.method == 'GET') {
       request.response
-        ..write(jsonEncode((jsonData)))
+        ..write(jsonEncode(result.toList()))
         ..close();
-
     }
   }
 }
