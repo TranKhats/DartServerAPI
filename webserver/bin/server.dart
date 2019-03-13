fa4a465326d5c68ef8540328b8196daa0909d27d
Tran Kha <35250985+TranKhats@users.noreply.github.com>
2019-03-13 11:39:34 +0700

    udate

--- webserver/bin/server.dart
+++ webserver/bin/server.dart
@@ -1,21 +1,21 @@
 import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
-import 'package:mysql1/mysql1.dart';
+//import 'package:mysql1/mysql1.dart';
 
 // $servername = "localhost";
 // $username = "root";
 // $password = "";
 
 Future main() async {
- var settings = new ConnectionSettings(
-  host: 'localhost', 
-  port: 3306,
-  user: 'root',
-  password: 'Khoikhats123',
-  db: 'test'
-);
-//var conn = await MySqlConnection.connect(settings);
+//  var settings = new ConnectionSettings(
+//   host: '207.148.71.41', 
+//   port: 8089,
+//   user: 'debian-sys-maint',
+//   password: 'bcVstnrLBTO2l3RO',
+//   db: 'demodb'
+// );
+// var conn = await MySqlConnection.connect(settings);
   //var result = await conn.query('select * from SinhVien');
   // for (var i = 0; i < result.toList().length; i++) {
     
