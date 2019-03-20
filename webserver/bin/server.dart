7eba857a4e26506f2d2ee82eca5a077259b89b39
Tran Kha <35250985+TranKhats@users.noreply.github.com>
2019-03-20 23:45:15 +0700

    try To parse to json from res of query

--- webserver/bin/server.dart
+++ webserver/bin/server.dart
@@ -1,40 +1,35 @@
 import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
-//import 'package:mysql1/mysql1.dart';
-
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
+import 'package:mysql1/mysql1.dart';
 
 Future main() async {
-//  var settings = new ConnectionSettings(
-//   host: '207.148.71.41', 
-//   port: 8089,
-//   user: 'debian-sys-maint',
-//   password: 'bcVstnrLBTO2l3RO',
-//   db: 'demodb'
-// );
-// var conn = await MySqlConnection.connect(settings);
-  //var result = await conn.query('select * from SinhVien');
-  // for (var i = 0; i < result.toList().length; i++) {
-    
-  // }
-  Map res={
-    'Id':'123',
-    'IsLogin':true
-  };
+  var settings = new ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'Khoikhats123',
+      db: 'coffemangement');
+  var conn = await MySqlConnection.connect(settings);
+
   var server = await HttpServer.bind(
     InternetAddress.loopbackIPv4,
     5050,
   );
   print('Listening on localhost:${server.port}');
 
-  await for (HttpRequest request in server) {
-    if (request.method == 'GET') {
-      request.response
-        //..write(jsonEncode(result.toList()))
-        ..write(jsonEncode(res))
+  await for (HttpRequest req in server) {
+    if (req.method == 'POST' &&
+        req.headers.contentType.toString() == 'application/json') {
+      String content = await req.transform(utf8.decoder).join();
+      var data = jsonDecode(content) as Map; /*3*/
+      String sql =
+          "select * from user where username='${data['username']}' and password='${data['password']}'";
+      var result = await conn.query(sql);
+      //var map = Map<String, dynamic>.from();
+     
+      req.response
+        ..write(jsonEncode(result.toList()))
         ..close();
     }
   }
