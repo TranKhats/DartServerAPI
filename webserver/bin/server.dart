bd85b5a8d0440cb2c2064de65dea7e834f552167
Tran Kha <35250985+TranKhats@users.noreply.github.com>
2019-03-22 13:54:11 +0700

    try logining
    un comment to fetch data

--- webserver/bin/server.dart
+++ webserver/bin/server.dart
@@ -4,6 +4,14 @@ import 'dart:convert';
 import 'package:mysql1/mysql1.dart';
 import 'AccountModel.dart';
 
+List<Map<String,dynamic>> convertToMapUser(Results result){
+  List<Map<String,dynamic>> users=new List();
+  for (var row in result) {
+    users.add(new AccountModel(row[0], row[1].toString()).toJson());
+  }
+  return users;
+}
+
 Future main() async {
   var settings = new ConnectionSettings(
       host: '207.148.71.41',
@@ -19,5 +27,17 @@ Future main() async {
   );
   print('Listening on localhost:${server.port}');
 
-
+  await for (HttpRequest req in server) {
+    if (req.method == 'POST' &&
+        req.headers.contentType.toString() == 'application/json') {
+      String content = await req.transform(utf8.decoder).join();
+      var data = jsonDecode(content) as Map; /*3*/
+      String sql =
+          "select * from user where username='${data['username']}' and password='${data['password']}'";
+      var result = await conn.query(sql);
+      req.response
+        ..write(jsonEncode(convertToMapUser(result)))
+        ..close();
+    }
+  }
 }
